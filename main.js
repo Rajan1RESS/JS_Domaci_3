5f253f495750b3e7465e0bbe9ec60d829da77c05
Rajan <rajanmiletic7@gmail.com>
2023-02-05 13:39:36 +0100

    main page

---
+++ main.js
@@ -0,0 +1,15 @@
+const dugme1 = document.querySelector("#zad1");
+const dugme2 = document.querySelector("#zad2");
+const dugme3 = document.querySelector("#zad2");
+
+dugme1.addEventListener("click", () => {
+    location.href = ""
+});
+
+dugme2.addEventListener("click", () => {
+    location.href = "";
+})
+
+dugme2.addEventListener("click", () => {
+    location.href = "";
+})
