25c853736b86198c073f39cd3aef958371c3e5f0
Rajan <rajanmiletic7@gmail.com>
2023-02-08 23:00:25 +0100

    2. zad pocetak

--- zadatak2/script.js
+++ zadatak2/script.js
@@ -7,8 +7,19 @@ const sub_dugme = document.getElementById("sub_dugme");
 
 sub_dugme.addEventListener("click", () => {
     let vrijednost = klik_dugmeta();
-    console.log(vrijednost);
+    napraviPolja(vrijednost);
+    // box.addEventListener("input",()=>{});
 });
 
-
+function napraviPolja(broj_kockica){
+    let board = document.getElementById("board");
+    for(let i=0;i<broj_kockica;i++){
+        let box = document.createElement("div");
+       
+        // let node = document.createTextNode ('Hello, I am Arun');
+        // box.appendChild("p")
+        box.className = "box-element";
+        board.appendChild(box);
+    }
+}
 
