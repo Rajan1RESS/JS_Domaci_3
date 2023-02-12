6ed508854926e8fd1b94a3c015257c3c48107c23
Rajan <rajanmiletic7@gmail.com>
2023-02-12 15:41:43 +0100

    fix

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const dugme2 = document.querySelector("#zad2");
 const dugme3 = document.querySelector("#zad3");
 
 dugme1.addEventListener("click", () => {
-    window.location = "/zadatak1/index.html";
+    window.location = "/zadatak1/prvi.html";
 });
 
 dugme2.addEventListener("click", () => {
-    window.location = "/zadatak2/index.html";
+    window.location = "/zadatak2/prvi.html";
 });
 
 dugme3.addEventListener("click", () => {
-    location.href = "/zadatak3/index.html";
+    location.href = "/zadatak3/prvi.html";
 })
 
